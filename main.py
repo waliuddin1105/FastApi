18e2f3e5f167702841c5cf0416134e11d16da148
waliuddin1105 <waliahmed1105@gmail.com>
2026-01-18 16:01:34 +0500

    Learning request body, post requests and pydantic BaseModel

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
 @app.get('/home')
 def index():
     return {"data" : {"Name" : "wali"}}
+
+@app.get('/blog')
+def blogs(limit : int = 10, published : bool = True, sort  : Optional[str] = None):
+    if published:
+        return {"data" : f"{limit} blogs that have been published"}
+    else:
+        return{"data" : "No published blogs available"}
 
 @app.get('/blog/unpublished')
 def unpublished():
@@ -18,3 +27,11 @@ def getBlog(id : int):
 def comments(id : int):
     return {"data" :  {'1','2'}}
     
+class Blog(BaseModel):
+    title : str
+    body : str
+    published : Optional[bool]
+
+@app.post('/blog')
+def CreateBlog(data : Blog):
+    return {"Success" : f"Blog with title {data.title} created!"}
